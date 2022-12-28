738e3ca9a838623269f56669ca077069a06b583f
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-29 00:51:13 +0200

    add files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// import articlesTpl from './templates/articles.hbs';
+import './css/common.css';
+// import NewsApiService from './js/news-service';
+// import LoadMoreBtn from './js/components/load-more-btn';
